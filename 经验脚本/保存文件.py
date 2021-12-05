db0f1c725e32db576d4a7a99f6fc8595a2243ae7
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-12-05 15:44:04 +0800

    Create 保存文件.py

---
+++ 经验脚本/保存文件.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.savefig(fname='hist.png', dpi=300,bbox_inches='tight',pad_inches=0.1)
+
+# bbox_inches 以英寸为单位的边界框
+# 以英寸为单位的边界框 pad_inches 边框到边界之间留存的空间 默认 0.1 英寸inches
