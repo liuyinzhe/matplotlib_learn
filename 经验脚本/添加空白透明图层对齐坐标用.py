b7d612c8fc035334ede759bbe4165c8fd5a9091a
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-01-06 16:49:34 +0800

    Update 添加空白透明图层对齐坐标用.py

--- 经验脚本/添加空白透明图层对齐坐标用.py
+++ 经验脚本/添加空白透明图层对齐坐标用.py
@@ -1,13 +1,15 @@
-    #透明
-    #alpha=0
-    
-    #图层绘画前后顺序
-    #zorder=0
+import numpy as np
+import matplotlib.pyplot as plt
+#透明
+#alpha=0
 
-    #添加空白axes
-    #[left, bottom, width, height] ,为axes 坐标轴起点在figure 上的起点x,y
-    ax = fig.add_axes([0.4,0.5,1,1],alpha=0,zorder=0,)
-    ax.axis('off')
-    # 优雅的给所有ax 设置关闭坐标轴
-    #fig, axes = plt.subplots(col, row)
-    #np.vectorize(lambda ax:ax.axis('off'))(axes)
+#图层绘画前后顺序
+#zorder=0
+
+#添加空白axes
+#[left, bottom, width, height] ,为axes 坐标轴起点在figure 上的起点x,y
+ax = fig.add_axes([0.4,0.5,1,1],alpha=0,zorder=0,)
+ax.axis('off')
+# 优雅的给所有ax 设置关闭坐标轴
+#fig, axes = plt.subplots(col, row)
+#np.vectorize(lambda ax:ax.axis('off'))(axes)
