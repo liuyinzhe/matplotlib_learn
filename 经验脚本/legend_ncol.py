f5db4eb9c558f7c28b39a5878e59050d0adb5d05
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-05-15 18:42:51 +0800

    Create legend_ncol.py

---
+++ 经验脚本/legend_ncol.py
@@ -0,0 +1,27 @@
+
+#https://www.zhihu.com/question/405000243/answer/2338197484
+ 
+plt.legend(ncol=2)
+
+'''
+plt.legend(loc='best')。其中loc参数可选：
+0:  'best'
+1:  'upper right'
+2:  'upper left' 
+3:  'lower left'
+4:  'lower right'
+5:  'right'
+6:  'center left'
+7:  'center right'
+8:  'lower center'
+9:  'upper center'
+10:  'center'
+
+(1)  fontsize：int or float.   设置图例文字大小；
+(2)  frameon：True or False.   是否显示图例边框；
+(3)  edgecolor：设置图例边框颜色；
+(4)  ncol：设置图例中条目的列数；
+(5)  columnspacing：设置列间距；
+(6)  labelspacing：设置图例条目之间的垂直距离；
+(7)  title：设置图例的标题；
+'''
